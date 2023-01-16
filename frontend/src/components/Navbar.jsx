0ae0de2762eadca942a922e4e7f6f339557c1d0f
philip <mertz@stud.fra-uas.de>
2023-01-16 19:31:17 +0100

    greenframe

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,8 +10,7 @@ import {
     MenuItem,
     MenuList
 } from "@chakra-ui/react"
-//import Logo from "../images/logo-simplify.png"
-import Logo from "../images/logo-simplify.svg"
+import Logo from "../images/logo-simplify.png"
 import { HiMenu, HiOutlineLogout } from "react-icons/hi";
 import { useContext, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
